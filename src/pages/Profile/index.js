6b74bc6d2f6e277d9f1f7112e79ef4e31052aa4c
carlosberti <carlosberti1@hotmail.com>
2019-07-25 21:49:41 -0300

    first commit

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+
+import { Container } from './styles';
+
+export default function Profile() {
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+        <hr />
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
+}
